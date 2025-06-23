40c807e11a0f4919f5e7af3f433a9ae7fc699693
Twinkym <twinkymen@hotmail.com>
2025-06-23 03:47:25 +0200

    Creados login y registro junto con el DTO y el authController

---
+++ src/main/java/com/grupo4/restaurante/dto/UsuarioRegistroDTO.java
@@ -0,0 +1,22 @@
+package com.grupo4.restaurante.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UsuarioRegistroDTO {
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    private String password;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ser un email válido")
+    private String email;
+
+    private String rol = "CLIENTE";  //Valor por defecto.
+
+}
