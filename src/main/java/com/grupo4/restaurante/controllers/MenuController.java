c7429326a05143ee23ac7699919101a393582569
Twinkym <twinkymen@hotmail.com>
2025-06-26 02:31:06 +0200

    creación de layout, contacto, renderización de reservas.

--- src/main/java/com/grupo4/restaurante/controllers/MenuController.java
+++ src/main/java/com/grupo4/restaurante/controllers/MenuController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Menu Controller.
  * @author David De La Puente
- * @author Alejandro
  * @author Luis Miguel
  * @version 1.0
  * @since 2025-05-28
